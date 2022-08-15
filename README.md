7586a8cd8affdc235f6fb8f9f3542c66fc6f281e
Thom van der Woude <tbvanderwoude@gmail.com>
2022-08-15 14:47:59 +0200

    Removed miscellaneous logic

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # grid_util
-Collection of utility constructs like grids, points, rectangles, etc. Also includes some miscellaneous logic
-like [serde](https://serde.rs/) file IO helper functions.
+Collection of utility constructs like grids, points, rectangles, etc.
--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,7 @@
 //! Collection of utility constructs like [Grid](grid::Grid)'s, [Point](point::Point)'s, [Rect](rect::Rect)'s, etc. Also includes some miscellaneous logic
 //! like [serde] file IO helper functions.
 
-use std::cmp::Ordering;
-
 pub mod direction;
 pub mod grid;
-pub mod io;
 pub mod point;
 pub mod rect;
-
-/// Takes a primary and secondary [Ordering] and evaluates it to a single [Ordering] ([Source](https://stackoverflow.com/questions/40369255/reverse-specific-key-when-sorting-with-multiple-keys)).
-pub fn chain_ordering(o1: Ordering, o2: Ordering) -> Ordering {
-    match o1 {
-        Ordering::Equal => o2,
-        _ => o1,
-    }
-}
